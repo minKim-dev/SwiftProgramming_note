53b95155d7bb782022f970095f39d5735ee49486
luvMint <lovejd0815@gmail.com>
2023-10-19 00:28:05 +0900

    chapter20 SwiftUI로 커스텀 뷰 생성하기 관련 파일 만듬.(오늘은 시험기간으로 딱히 Swift에 쏟을 시간이 없었음...)

---
+++ LearningSwift/GeneratingCustomView.swift
@@ -0,0 +1,23 @@
+//
+//  GeneratingCustomView.swift
+//  LearningSwift
+//
+//  Created by 김민성 on 2023/10/19.
+//
+
+/*:
+ 1. 스위프트에서 View는 어떠한 의미를 가지는가?
+ */
+import SwiftUI
+
+struct GeneratingCustomView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GeneratingCustomView_Previews: PreviewProvider {
+    static var previews: some View {
+        GeneratingCustomView()
+    }
+}
