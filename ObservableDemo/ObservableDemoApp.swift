f25a7bf8c582f9afd3dcd5acb44a8e1ef2367391
luvMint <lovejd0815@gmail.com>
2023-11-07 17:37:07 +0900

    Initial Commit

---
+++ ObservableDemo/ObservableDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservableDemoApp.swift
+//  ObservableDemo
+//
+//  Created by 김민성 on 2023/11/07.
+//
+
+import SwiftUI
+
+@main
+struct ObservableDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
